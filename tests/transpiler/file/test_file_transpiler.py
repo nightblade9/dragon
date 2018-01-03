5485ed2ed5673ebda139dad599620f590bf5a37e
nightblade9 <nightbladecodes@gmail.com>
2018-01-03 01:04:11 -0500

    Add placeholder file_transpiler tests

---
+++ tests/transpiler/file/test_file_transpiler.py
@@ -0,0 +1,10 @@
+import unittest
+from dragon.transpiler.file.file_transpiler import FileTranspiler
+
+class TestFileTranspiler(unittest.TestCase):
+    def test_transpile_transpiles_main_py_to_haxe(self):
+        # TODO: 
+        # 1) Load main.py that's been "pythonified"
+        # 2) Convert it to main.hx via transpiler
+        # 3) Qualitatively assess if everything is good
+        pass
