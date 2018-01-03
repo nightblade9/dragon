27c492ecbaa25847fb9bff6bf37fe69886002f90
nightblade9 <nightbladecodes@gmail.com>
2018-01-02 21:30:49 -0500

    Redo: now Python => Haxe as expected Rewrote code to add package statement Disabled import transformation for now

--- dragon/transpilation/file_transpiler.py
+++ dragon/transpilation/file_transpiler.py
@@ -1,32 +1,45 @@
+import os
 import re
 
 class FileTranspiler:
 
-    # Line-level regular expressions!
-    _PACKAGE_REGEX = "^package.*;"
-    _IMPORT_REGEX = "^import ([a-zA-Z]+)\.([a-zA-Z\.]);"
+    # We only support newstyle "from x import y". Applied line by line.
+    _IMPORT_REGEX = r"^from ([a-zA-Z\.]+) import ([a-zA-Z]+);"
     
-    def transpile(self, haxe_code):
+    def __init__(self, filename):
+        self._filename = filename
+
+    def transpile(self):
+        with open(self._filename, "rt") as file:
+            python_code = file.read()
+
         # NB: the first transpile operation causes the rest of regex matches to fail
         # Probably something to do with newlines
-        python_code = haxe_code
-        python_code = self._remove_packages(python_code)
-        python_code = self._transform_imports(python_code)
-        return python_code
+        code = python_code
+        code = self._add_package_statement(code)
+        code = self._transform_imports(code)
+        return code
 
-    def _remove_packages(self, haxe_code):
-        code_lines = haxe_code.splitlines()
-        output = ""
-        for line in code_lines:
-            output = "{}\n{}".format(output, re.sub(FileTranspiler._PACKAGE_REGEX, "", line))
-        return output
+    def _add_package_statement(self, code):
+        path_only = os.path.split(self._filename)[0]
+        packages = path_only.split(os.path.sep)
+        package_statement = "package"
 
-    def _transform_imports(self, haxe_code):
-        code_lines = haxe_code.splitlines()
+        if len(packages) and packages != [""]:
+            package_statement = "package {}".format(".".join(packages))
+
+        package_statement = "{};".format(package_statement)
+        code = "{}\n{}".format(package_statement, code)
+        return code
+
+    def _transform_imports(self, code):
+        return code # TODO: FIX
+
+        code_lines = code.splitlines()
         output = ""
         for line in code_lines:
             match = re.search(FileTranspiler._IMPORT_REGEX, line)
-            print("{} => {}".format(line, match))
+            #print("{} => {}".format(line, match))
             if match:
                 print(match)
                 pass
--- tests/transpiler/test_file_transpiler.py
+++ tests/transpiler/test_file_transpiler.py
@@ -1,13 +1,15 @@
-import re
+import distutils.dir_util
+import os
 import unittest
 from dragon.transpilation.file_transpiler import FileTranspiler
 
 class TestFileTranspiler(unittest.TestCase):
     ###
-    # A series of "integration tests" that transpile the HaxeFlixel template (as of 4.3.0)
+    # A series of "integration tests" that transpile the pythonified 
+    # HaxeFlixel template (as of 4.3.0)
     ###
 
-    MAIN_HX = """package;
+    MAIN_HX_EXPECTED = """package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
@@ -22,7 +24,7 @@ class Main extends Sprite
 }
 """
 
-    MAIN_HX_AS_PYTHON = """
+    MAIN_HX_PYTHON = """
 from flixel.flx_game import FlxGame
 from openfl.display.sprite import Sprite
 
@@ -32,18 +34,32 @@ class Main(Sprite):
         add_child(FlxGame(0, 0, PlayState))
 """
 
-    def test_transpile_removes_package_statements(self):
-        t = FileTranspiler()
-        python_code = t.transpile(TestFileTranspiler.MAIN_HX)
-        self.assertIsNone(re.search(FileTranspiler._PACKAGE_REGEX, python_code))
+    def test_transpile_adds_empty_package_statement_for_root_file(self):
+        with open("main.py", "wt") as file:
+            file.write(TestFileTranspiler.MAIN_HX_PYTHON)
+        try:
+            t = FileTranspiler("main.py")
+            haxe_code = t.transpile()
+            self.assertTrue("package;" in haxe_code)
+        except:
+            os.remove("main.py")
+            raise
 
-    def test_transpile_transforms_imports_into_python_style(self):
-        t = FileTranspiler()
-        python_code = t.transpile(TestFileTranspiler.MAIN_HX)
+    def test_transpile_adds_dot_delimited_pacakge_for_deep_file(self):
+        file_path = os.path.join("deengames", "owlicious", "core", "element.py")
+        distutils.dir_util.create_tree(file_path[0:-1], file_path[-1])
 
-        haxe_imports = re.search(FileTranspiler._IMPORT_REGEX, python_code)
-        self.assertIsNone(haxe_imports)
-
-        # Why isn't this failing? We didn't remove anything yet.
+        with open(file_path, "wt") as file:
+            file.write(TestFileTranspiler.MAIN_HX_PYTHON)
+        try:
+            t = FileTranspiler(file_path)
+            haxe_code = t.transpile()
+            self.assertTrue("package deengames.owlicious.core" in haxe_code)
+            self.assertTrue("package deengames.owlicious.core.element" not in haxe_code)
+        except:
+            path_only = os.path.split(file_path)[0]
+            os.remove(file_path)
+            distutils.dir_util.remove_tree(path_only)            
+            raise
 
     ### End series ###
