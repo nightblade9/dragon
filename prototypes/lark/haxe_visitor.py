5cbe183627705a06a25e7920801c7452ffebbb57
nightblade9 <nightbladecodes@gmail.com>
2018-01-09 17:05:46 -0500

    Visitor pattern. Output didn't change.

--- prototypes/lark/haxe_visitor.py
+++ prototypes/lark/haxe_visitor.py
@@ -1,11 +1,34 @@
 from lark.tree import Visitor
 
 class HaxeVisitor(Visitor):
+    
+    LAST_NODE = None
+
     def process(self, tree):
         return self.visit(tree)
     
-    def import_stmt(self, data):
-        print("IMPORT {}".format(data))
-    
-    def compound_stmt(self, data):
-        print("COMP".format(data))
+    def import_from(self, node):
+        print("@@@@@ NODE {}".format(node))
+        output = "import "
+        HaxeVisitor.LAST_NODE = node
+
+        #node = node.children[0].children[0] # import_stmt => import_from
+        # package_name = node.children[0].children[0].value
+
+        # x = """
+        # >>> node.children[0].children[0].value
+        # 'flixel'
+        # >>> node.children[1].children[0].children[0].value
+        # 'FlxGame'
+        # """
+
+        package_tree = node.children[0].children
+        class_name = node.children[1].children[0].children[0].value
+
+        for package_node in package_tree:
+            output = "{}{}.".format(output, package_node.value)
+
+        output = "{}{}".format(output, class_name)
+
+        print("i={} o={}".format(node, output))
+        return output
