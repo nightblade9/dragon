566449bb0e3932258f6a49ca71759f8195d4087b
nightblade9 <nightbladecodes@gmail.com>
2018-01-12 22:34:44 -0500

    First cut at a Lark validator. No unit tests yet.

--- dragon/transpiler/lark/lark_transpiler.py
+++ dragon/transpiler/lark/lark_transpiler.py
@@ -1,8 +1,9 @@
 from lark import Lark
-import os
 from .python_indenter import PythonIndenter
-import sys
 from .transformers.haxe_transformer import HaxeTransformer
+from .validators.lark_validator import LarkValidator
+import os
+import sys
 
 class LarkTranspiler:
     
@@ -17,9 +18,9 @@ class LarkTranspiler:
         
         grammar_path = sys.modules[__name__].__file__
         grammar_path = grammar_path[:grammar_path.rindex(os.path.sep)]
-        grammar_filename = os.path.join(grammar_path, LarkTranspiler._PYTHON_3_GRAMMAR_FILENAME)
+        self.grammar_filename = os.path.join(grammar_path, LarkTranspiler._PYTHON_3_GRAMMAR_FILENAME)
 
-        with open(grammar_filename) as f:
+        with open(self.grammar_filename) as f:
             self._python_parser = Lark(f, parser=LarkTranspiler._PARSER, postlex=PythonIndenter(), start=LarkTranspiler._PARSER_START)
 
     def transpile(self):
@@ -31,6 +32,11 @@ class LarkTranspiler:
             except:
                 print ('Failure parsing %s' % filename)
                 raise
+
+            validator = LarkValidator(self.grammar_filename)
+            if not validator.is_fully_parsed(tree):
+                #raise NotImplementedError
+                print("{} is not fully parsable.".format(filename))
 
 def _convert_and_print(tree, filename):
     filename = filename.replace('.py', '.hx')
---
+++ dragon/transpiler/lark/validators/lark_validator.py
@@ -0,0 +1,39 @@
+from lark import Tree
+import re
+
+class LarkValidator:
+    
+    _TOKEN_REGEX = r"^([a-zA-Z0-9_\-]+):"
+
+    def __init__(self, grammar_filename):
+        self._grammar_tokens = self._extract_tokens(grammar_filename)
+
+    def is_fully_parsed(self, tree):
+        if isinstance(tree, str) and tree in self._tokens:
+            print("Found an unprocessed token: {}".format(tree))
+            return False
+        elif isinstance(tree, Tree):
+            if tree.data in self._tokens:
+                print("Found an unprocessed subtree: {}".format(tree.data))
+                return False
+            if len(tree.children):
+                for node in tree.children:
+                    if not self.is_fully_parsed(node):
+                        print("Found an unprocessed node: {}".format(node.data))
+                        return False
+        
+        return True
+             
+
+    def _extract_tokens(self, grammar_filename):
+        with open(grammar_filename, "rt") as f:
+            grammar = f.read()
+
+        lines = grammar.splitlines()
+        self._tokens = []
+
+        for line in lines:
+            match = re.search(LarkValidator._TOKEN_REGEX, line)
+            if match:
+                token = match.group(1)
+                self._tokens.append(token)
