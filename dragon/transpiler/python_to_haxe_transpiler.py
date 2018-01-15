3dd7cade54c5ebf4ccefc2bfdb25f41383bb1cca
nightblade9 <nightbladecodes@gmail.com>
2018-01-14 23:53:58 -0500

    package statement tests Fixed bugs with some cases Tested multiple OS path styles!

--- dragon/transpiler/python_to_haxe_transpiler.py
+++ dragon/transpiler/python_to_haxe_transpiler.py
@@ -20,11 +20,13 @@ class PythonToHaxeTranspiler:
         with open(filename, 'wt') as f:
             f.write(code)
 
-def _add_package_statement(source_root, path_and_filename, code):
+def _add_package_statement(source_root, path_and_filename, code, path_separator=os.path.sep):
     # eg. source_root = template\source, filename = template\source\main.py
     # Resulting package should be empty string. Subtract the two strings.
-    filename = path_and_filename[0:path_and_filename.rindex(os.path.sep)]
+    filename = path_and_filename[0:path_and_filename.rindex(path_separator)]
     filename = filename.replace(source_root, "")
-    package = filename.replace(os.path.sep, ".")
+    # Sometimes the first character is a path separator that we don't want
+    filename = filename[1:] if len(filename) and filename[0] == path_separator else filename
+    package = filename.replace(path_separator, ".")
     code = "package {};\n{}".format(package, "\n".join(code))
     return code
