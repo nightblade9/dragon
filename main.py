e2b9b8de9636e3b52403f356db43cb5496d84221
nightblade9 <nightbladecodes@gmail.com>
2018-01-18 12:38:08 -0500

    Restructured Mars code for compiling Fixed issue with recursive file search Cleaned up output with print statements

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ from dragon.transpiler.python_to_haxe_transpiler import PythonToHaxeTranspiler
 from dragon.template_creator import TemplateCreator
 
 TEMPLATE_COMMAND = "template"
-VALID_COMMANDS = [TEMPLATE_COMMAND]
+BUILD_COMMAND = "build"
+VALID_COMMANDS = (TEMPLATE_COMMAND, BUILD_COMMAND)
 
-def dragon(args):
-    # Debug mode!
-    if len(args) == 0:
-        # Just transpile in-place for now.
-        source_path = os.path.join("template", "source")
-        files = glob.glob(os.path.join(source_path, "**/*.py"))
-        PythonToHaxeTranspiler(source_path, files).transpile()
-    elif len(args) >= 1:
+def mars(args):
+    if len(args) >= 1:
         command = args[0]
         if command == TEMPLATE_COMMAND:
             if len(args) >= 2:
@@ -26,11 +21,20 @@ def dragon(args):
                 template_creator = TemplateCreator()
             template_creator.create_template()
             print("Template project created in {}".format(template_creator.output_directory))
+        elif command == BUILD_COMMAND:
+            if len(args) >= 2:
+                source_path = args[1]
+                files_path = os.path.join(os.getcwd(), source_path, "**/*.py")
+                files = glob.glob(files_path, recursive=True)
+                print("Transpiling {} files...".format(len(files)))
+                PythonToHaxeTranspiler(source_path, files).transpile()
+            else:
+                print("Please specify the target directory to build.")
         else:
             print("'{}' is not a valid command. Valid commands are: {}".format(command, VALID_COMMANDS))
     else:
-        print("Usage: python cmdline.py {}".format(VALID_COMMANDS))
+        print("Usage: python main.py {}".format(VALID_COMMANDS))
 
 if __name__ == "__main__":
     args = sys.argv[1:] if len(sys.argv) > 1 else []
-    dragon(args)
+    mars(args)
