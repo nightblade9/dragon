6d11babb81b243776924072a2bd4cea4edcb087c
nightblade9 <nightbladecodes@gmail.com>
2018-01-18 13:15:49 -0500

    Rip out Mars code.

--- template/source/main.py
+++
@@ -1,8 +0,0 @@
-from flixel import FlxGame
-from openfl.display import Sprite
-import PlayState
-
-class Main(Sprite):
-    def __init__(self):
-        super(Sprite, self).__init__()
-        self.addChild(FlxGame(0, 0, PlayState))
