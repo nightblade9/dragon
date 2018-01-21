da8e2b06e5492cd78ea32cc76fef0fd631ed5463
nightblade9 <nightbladecodes@gmail.com>
2018-01-20 22:16:14 -0500

    Fix fully-parsed validation check Slight tweak to grammar for consistency

--- dragon/transpiler/lark/validators/lark_validator.py
+++ dragon/transpiler/lark/validators/lark_validator.py
@@ -3,25 +3,18 @@ import re
 
 class LarkValidator:
     
-    _TOKEN_REGEX = r"^([a-zA-Z0-9_\-]+):"
+    _TOKEN_REGEX = r"^([a-zA-Z0-9_\-\?!_\.]+):"
 
     def __init__(self, grammar_filename):
         self._grammar_tokens = self._extract_tokens(grammar_filename)
 
-    def is_fully_parsed(self, tree):
-        if isinstance(tree, str) and tree in self._tokens:
-            print("Found an unprocessed token: {}".format(tree))
-            return False
-        elif isinstance(tree, Tree):
-            if tree.data in self._tokens:
-                print("Found an unprocessed subtree: {}".format(tree.data))
-                return False
-            if len(tree.children):
-                for node in tree.children:
-                    if not self.is_fully_parsed(node):
-                        print("Found an unprocessed node: {}".format(node.data))
-                        return False
-        
+    def is_fully_parsed(self, code):
+        for line in code:
+            for token in self._grammar_tokens:
+                if token in line:
+                    print("Found unparsed token {} in line {}".format(token, line))
+                    return False
+            
         return True
              
     def _extract_tokens(self, grammar_filename):
@@ -29,13 +22,17 @@ class LarkValidator:
             grammar = f.read()
 
         lines = grammar.splitlines()
-        self._tokens = []
+        tokens = []
 
         for line in lines:
             match = re.search(LarkValidator._TOKEN_REGEX, line)
             if match:
-                token = match.group(1)
-                self._tokens.append(token)
+                token = match.group(1)                
+                token_name = token.replace("?", "").replace("!", "")
+                tokens.append(token_name)
+        
+        return tokens
+                
 
 def validate_class_definition(class_name, base_classes):
     if len(base_classes) > 1:
